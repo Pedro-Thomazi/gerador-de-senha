8f6337aa8d7d8ddd43b98857ef44f216f407c901
Pedro T <pedro.thomazi@hotmail.com>
2024-01-24 14:25:06 -0300

    Add mais caracteres

--- js/index.js
+++ js/index.js
@@ -9,7 +9,12 @@ const caracteres = [
   'y', 'z', '1', '2', '3', '4',
   '5', '6', '7', '8', '9', '0',
   '!', '@', '#', '$', '%', '^',
-  '&', '*', '?'
+  '&', '*', '?', 'A', 'B', 'C',
+  'D', 'E', 'F', 'G', 'H', 'I',
+  'J', 'K', 'L', 'M', 'N', 'O',
+  'P', 'Q', 'R', 'S', 'T', 'U',
+  'V', 'W', 'X', 'Y', 'Z', '-',
+  '_', '=', '+', '<', '>'
 ]
 
 form.addEventListener('submit', (e) => {
@@ -36,7 +41,7 @@ form.addEventListener('submit', (e) => {
   finalPassword.innerText = palavra
 })
 
-finalPassword.addEventListener('click', () =>  {
+finalPassword.addEventListener('click', () => {
   navigator.clipboard.writeText(finalPassword.innerText).then(() => {
     alert("Senha Copiada")
   })
